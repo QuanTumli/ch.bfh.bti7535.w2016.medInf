ac8b9338cd72c34dc429e8467ca81109c69e0bfa
Kevin Tippenhauer <kevin.m.t@hotmail.com>
2017-01-11 13:32:19 +0100

    little cleanup in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -33,71 +33,74 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
-		// tokenize files
-		// Tokenizer tokenizer = context.getBean(Tokenizer.class);
-		// TokenizationHelper.tokenizeFilesInFolder(path + "train/neg",
-		// tokenizer);
-		// TokenizationHelper.tokenizeFilesInFolder(path + "train/pos",
-		// tokenizer);
-		//// tokenize files
+
 		List<Tokenizer> tokenizers = (List<Tokenizer>) context.getBean("getTokenizer");
 		List<Analyzer> analyzers = (List<Analyzer>) context.getBean("getAnalyzer");
 		TokenizationHelper tokenizationHelper = context.getBean(TokenizationHelper.class);
-		
-		
+
 		for (String path : paths) {
 			System.out.println();
 			System.out.println("Starting with: " + path);
 			for (Tokenizer tokenizer : tokenizers) {
 				System.out.println("\tStarting with:" + tokenizer.getClass().getSimpleName());
 
-//				tokenizationHelper.tokenizeFilesInFolder(path + "train/neg", tokenizer);
-//				tokenizationHelper.tokenizeFilesInFolder(path + "train/pos", tokenizer);
-////
-////				// remove stopwords from files
-//				String subPath = path + "train/tokenized/" + tokenizer.getClass().getSimpleName();
-//				StopWordHelper.removeStopWordsForFilesInFolder(subPath + "/pos", tokenizer);
-//				StopWordHelper.removeStopWordsForFilesInFolder(subPath + "/neg", tokenizer);
-////
-////				// map all equal words
-//				subPath = path + "train/removedStopwords/" + tokenizer.getClass().getSimpleName();
-//				WordMappingHelper.mapWordsFromFilesInFolder(subPath + "/pos", tokenizer);
-//				WordMappingHelper.mapWordsFromFilesInFolder(subPath + "/neg", tokenizer);
+//				 tokenizationHelper.tokenizeFilesInFolder(path + "train/neg",
+//				 tokenizer);
+//				 tokenizationHelper.tokenizeFilesInFolder(path + "train/pos",
+//				 tokenizer);
+//				
+//				// remove stopwords from files
+//				 String subPath = path + "train/tokenized/" +
+//				 tokenizer.getClass().getSimpleName();
+//				 StopWordHelper.removeStopWordsForFilesInFolder(subPath +
+//				 "/pos", tokenizer);
+//				 StopWordHelper.removeStopWordsForFilesInFolder(subPath +
+//				 "/neg", tokenizer);
+//				
+//				// map all equal words
+//				 subPath = path + "train/removedStopwords/" +
+//				 tokenizer.getClass().getSimpleName();
+//				 WordMappingHelper.mapWordsFromFilesInFolder(subPath + "/pos",
+//				 tokenizer);
+//				 WordMappingHelper.mapWordsFromFilesInFolder(subPath + "/neg",
+//				 tokenizer);
 
 				Map<String, Integer> negatives = FileHelper.readFileToMap(
 						Paths.get(path + "/train/wordMapped/" + tokenizer.getClass().getSimpleName() + "/neg.csv"));
 				Map<String, Integer> positives = FileHelper.readFileToMap(
 						Paths.get(path + "/train/wordMapped/" + tokenizer.getClass().getSimpleName() + "/pos.csv"));
-				
-				negatives.entrySet().removeIf(e-> e.getValue() < 300 || e.getValue() > 3000);
 
-				positives.entrySet().removeIf(e-> e.getValue() < 300 || e.getValue() > 3000);
-				
-				ArrayList<String> removeList = new ArrayList<String>() {{
-					add("\'s");
-					add("film");
-					add("movie");
-					add("story");
-					add("time");
-					add("woman");
-					add("man");
-					add("women");
-					add("men");
-					add("character");
-					add("films");
-					add("people");
-					add("way");
-				}};
-				
-				negatives.entrySet().removeIf(e-> removeList.contains(e.getKey()));
-				positives.entrySet().removeIf(e-> removeList.contains(e.getKey()));
-				
+				negatives.entrySet().removeIf(e -> e.getValue() < 300 || e.getValue() > 3000);
+
+				positives.entrySet().removeIf(e -> e.getValue() < 300 || e.getValue() > 3000);
+
+				ArrayList<String> removeList = new ArrayList<String>() {
+					{
+						add("\'s");
+						add("film");
+						add("movie");
+						add("story");
+						add("time");
+						add("woman");
+						add("man");
+						add("women");
+						add("men");
+						add("character");
+						add("films");
+						add("people");
+						add("way");
+					}
+				};
+
+				negatives.entrySet().removeIf(e -> removeList.contains(e.getKey()));
+				positives.entrySet().removeIf(e -> removeList.contains(e.getKey()));
+
 				for (Analyzer analyzer : analyzers) {
 					System.out.println("\t\tStarting with: " + analyzer.getClass().getSimpleName());
 
 					analyzer.setNegativeWords(negatives);
 					analyzer.setPositiveWords(positives);
-//					analyzer.setTextModifiers(tokenizationHelper.getTextModifiers());
+					// analyzer.setTextModifiers(tokenizationHelper.getTextModifiers());
 					FileSorter fileSorter = new FileSorter(analyzer, tokenizer, tokenizationHelper.getTextModifiers());
 					float[] res = fileSorter.sort(path);
 					System.out.println("\t\t\tprecision: " + res[0] + " recall: " + res[1]);
@@ -114,76 +117,37 @@ public class Main {
 				for (String key : results.keySet()) {
 					if (key.contains(tokenizer.getClass().getSimpleName())
 							&& key.contains(analyzer.getClass().getSimpleName())) {
-						precision  += results.get(key)[0];
+						precision += results.get(key)[0];
 						recall += results.get(key)[1];
 						counter++;
 					}
 				}
-				
-				System.out.println(tokenizer.getClass().getSimpleName() + "\t" + analyzer.getClass().getSimpleName() + "\tPrecision: " + precision / counter);
-				System.out.println(tokenizer.getClass().getSimpleName() + "\t" + analyzer.getClass().getSimpleName() + "\tRecall: " + recall/counter);
+
+				System.out.println(tokenizer.getClass().getSimpleName() + "\t" + analyzer.getClass().getSimpleName()
+						+ "\tPrecision: " + precision / counter);
+				System.out.println(tokenizer.getClass().getSimpleName() + "\t" + analyzer.getClass().getSimpleName()
+						+ "\tRecall: " + recall / counter);
 			}
 		}
-		
-		
-//		for (Tokenizer tokenizer : tokenizers) {
-//			/* new try */
-//			//new TestTrainWithBayes(tokenizer);
-//			System.out.println("------------------" + tokenizer.getClass().getSimpleName() + "------------------");
-//			new TestTrainWithMyModel(tokenizer);
-//		}
-//		System.out.println("------------------" + tokenizers.get(0).getClass().getSimpleName() + "------------------");
-//		new TestTrainWithMyModel(tokenizers.get(0));
-		//
-		//// remove stopwords from files
-		// String subPath = path + "train/tokenized/" +
-		// tokenizer.getClass().getSimpleName();
-		// StopWordHelper.removeStopWordsForFilesInFolder(subPath + "/pos",
-		// tokenizer);
-		// StopWordHelper.removeStopWordsForFilesInFolder(subPath+ "/neg",
-		// tokenizer);
-		//
-		//// map all equal words
-		// subPath = path + "train/removedStopwords/" +
-		// tokenizer.getClass().getSimpleName();
-		// WordMappingHelper.mapWordsFromFilesInFolder(subPath + "/pos",
-		// tokenizer);
-		// WordMappingHelper.mapWordsFromFilesInFolder(subPath + "/neg",
-		// tokenizer);
 
-		/*
-		 * Analyzer analyzer = context.getBean(Analyzer.class); // // subPath =
-		 * path + "results/" + analyzer.getClass(); // FileSorter fileSorter =
-		 * new FileSorter(analyzer); fileSorter.sort(path);
-		 */
-		// JavaPlot p = new JavaPlot();
-		// p.addPlot("\"D:/Workspaces/Git/ch.bfh.bti7535.w2016.medInf/resources/test_data/set_0_99/train/wordMapped/SimpleTokenizer/mixed.txt\"
-		// using 2:xticlabels(1) with histogram");
-		//// "< sort
-		// C:/Users/quantumli/workspace/ch.bfh.bti7535.w2016.medInf/exercises/exercise1/pg1524_count.txt"
-		// using 2:xticlabels(1) with histogram
-		// p.plot();
-		// // subPath = path + "results/" + analyzer.getClass();
+		for (Tokenizer tokenizer : tokenizers) {
+			/* new try */
+			// new TestTrainWithBayes(tokenizer);
+			System.out.println("------------------" + tokenizer.getClass().getSimpleName() + "------------------");
+			new TestTrainWithMyModel(tokenizer);
+		}
+		// System.out.println("------------------" +
+		// tokenizers.get(0).getClass().getSimpleName() + "------------------");
+		// new TestTrainWithMyModel(tokenizers.get(0));
+
+		// String posWords = path + "train/wordMapped/" +
+		// tokenizer.getClass().getSimpleName() + "/pos.txt"; String negWords =
+		// path + "train/wordMapped/" + tokenizer.getClass().getSimpleName() +
+		// "/neg.txt"; String posTrain = path + "train/pos"; String negTrain =
+		// path + "train/neg";
 		//
-
-		/*
-		 * String posWords = path + "train/wordMapped/" +
-		 * tokenizer.getClass().getSimpleName() + "/pos.txt"; String negWords =
-		 * path + "train/wordMapped/" + tokenizer.getClass().getSimpleName() +
-		 * "/neg.txt"; String posTrain = path + "train/pos"; String negTrain =
-		 * path + "train/neg";
-		 * 
-		 * SentimentAnalyse sa = new SentimentAnalyse();
-		 * sa.trainFilesInFolder(posTrain, posWords, negTrain, negWords);
-		 */
-
-		// JavaPlot p = new JavaPlot();
-		// p.addPlot("\"D:/Workspaces/Git/ch.bfh.bti7535.w2016.medInf/resources/test_data/set_0_99/train/wordMapped/SimpleTokenizer/mixed.txt\"
-		// using 2:xticlabels(1) with histogram");
-		//// "< sort
-		// C:/Users/quantumli/workspace/ch.bfh.bti7535.w2016.medInf/exercises/exercise1/pg1524_count.txt"
-		// using 2:xticlabels(1) with histogram
-		// p.plot();
+		// SentimentAnalyse sa = new SentimentAnalyse();
+		// sa.trainFilesInFolder(posTrain, posWords, negTrain, negWords);
 
 	}
 
